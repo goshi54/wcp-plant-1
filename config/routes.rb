33fe6d0f48b7638e1dcdc7383a5b6a42c7da1f9a
g.o.s.h.i.cb400@gmail.com <ec2-user@ip-172-31-42-146.ap-northeast-1.compute.internal>
2021-12-11 16:11:04 +0000

    firstcommit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  devise_for :customers,skip: [:passwords,], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  root to: 'homes#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
